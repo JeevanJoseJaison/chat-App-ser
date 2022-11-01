2bebbbb8ccf1bace9a029cf1d76e9670df51bb4d
JeevanJoseJaison <66272323+JeevanJoseJaison@users.noreply.github.com>
2022-11-01 18:30:29 +0530

    Add files via upload

---
+++ public/precache-manifest.6da3d3ab071f8a41d4ba0bb0e2467423.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a94080d7b5eb45333dcc45db5326a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a9f744437cb5ff7507d",
+    "url": "/static/css/main.5d41ca5f.chunk.css"
+  },
+  {
+    "revision": "2ce654a2f7e705ed54b9",
+    "url": "/static/js/2.2e4432b2.chunk.js"
+  },
+  {
+    "revision": "6a9f744437cb5ff7507d",
+    "url": "/static/js/main.a3f7e2fe.chunk.js"
+  },
+  {
+    "revision": "b95af7ab8beceaf41336",
+    "url": "/static/js/runtime~main.2bbd149a.js"
+  }
+]);
